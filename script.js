67735c5edec1066cb1845224cde127391b11069c
Zlata Vasilenko <95431848+zlatavasilenko@users.noreply.github.com>
2022-07-11 20:24:13 +0300

    work

---
+++ script.js
@@ -0,0 +1,15 @@
+let swiper = new Swiper(".slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
